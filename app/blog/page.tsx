f63ebaf3bfa882f502e064607362636bd3bbe2db
AoiXD <oosabaaoi@gmail.com>
2025-03-08 17:08:59 +0900

    create_database

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,7 +4,8 @@ import Link from 'next/link';
 export default function BlogList() {
     return (
         <div className="container mx-auto px-4 py-8">
-            <h1 className="text-3xl font-bold mb-4">ブログ投稿一覧</h1>
+            <h1 className="text-3xl font-bold mb-4 mr-4 inline-block">ブログ投稿一覧</h1>
+            <Link href={"/blog/new"} className='text-lg px-4 py-2 bg-red-400 rounded-xl'>作成</Link>
             <ul className="space-y-4">
                 {posts.map((post) => (
                     <li key={post.id} className='border p-4 rounded-lg'>
